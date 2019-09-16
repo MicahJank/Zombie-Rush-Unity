643cb1c70bd2808e5c1ad730bbcb1f0bd092aec6
MicahJank <MicahJank@users.noreply.github.com>
2019-09-16 01:03:48 -0500

    created the Object script for the platform and added logic to make the platform move to the left

---
+++ Haunted Zombie Rush/Assets/Scripts/Object.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour {
+
+    [SerializeField] float objectSpeed = 4;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+    }
+}
