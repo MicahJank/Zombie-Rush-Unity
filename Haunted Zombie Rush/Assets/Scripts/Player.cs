7145a958b60dd7adbeaeb2239b0c0a72475577cc
MicahJank <MicahJank@users.noreply.github.com>
2019-09-16 23:43:49 -0500

    created logic for the jumping in the FixedUpdate

--- Haunted Zombie Rush/Assets/Scripts/Player.cs
+++ Haunted Zombie Rush/Assets/Scripts/Player.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    Animator anim;
+    [SerializeField] private float jumpforce = 100f;
+
+    private Animator anim;
+    private Rigidbody rigidbody;
+
+    private bool jump = false;
 
     // Start is called before the first frame update
     void Start() {
         anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
+        // checks to see if the left mouse button has been clicked, if it has then we can play the animation. Also we let the game know that the player has jumped by setting
+        // the jump bool to true and turning on the gravity.
         if(Input.GetMouseButtonDown(0)) {
             anim.Play("Jump");
+            rigidbody.useGravity = true;
+            jump = true;
+        }
+    }
+
+    // any time you are dealing with physics and force, you need to put that code into fixed update to fix issues with frame rates messing up the forces
+    void FixedUpdate() {
+        if(jump) {
+           jump = false; 
+           rigidbody.velocity = new Vector2(0, 0);
+           rigidbody.AddForce(new Vector2(0, jumpforce), ForceMode.Impulse);
         }
     }
 }
